0c086ef9c8bbd2acbabb233c3405eedbba25beb6
davidojo1144 <davco1144@gmail.com>
2025-07-28 06:18:14 +0100

    Habit Tracking Mobile App

--- Habit-Tracker/lib/auth-context.tsx
+++ Habit-Tracker/lib/auth-context.tsx
@@ -1,13 +1,20 @@
 import { createContext } from "react";
+import { Models } from "react-native-appwrite";
 
 type AuthContextType = {
-    
+    user: Models.User<Models.Preferences> | null
+    signUp: (email: string, password: string) => Promise<void>
+    signIn: (email: string, password: string) => Promise<void>
 }
 
-const AuthContext = createContext(undefined)
+const AuthContext = createContext<AuthContextType | undefined>(undefined)
 
 export function AuthProvider({children}: {children: React.ReactNode}){
-    return <></>
+    return (
+        <AuthContext.Provider
+        value={{user, signUp, signIn}}
+        ></AuthContext.Provider>
+    )
 }
 
 export function useAuth() {
