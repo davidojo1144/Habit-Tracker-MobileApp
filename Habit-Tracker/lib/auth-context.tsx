eeb52d39e9f510851969d9df5598f87a08df9245
davidojo1144 <davco1144@gmail.com>
2025-07-28 06:26:23 +0100

    Habit Tracking Mobile App

--- Habit-Tracker/lib/auth-context.tsx
+++ Habit-Tracker/lib/auth-context.tsx
@@ -1,19 +1,37 @@
 import { createContext } from "react";
-import { Models } from "react-native-appwrite";
+import { ID, Models } from "react-native-appwrite";
+import { account } from "./appwrite";
 
 type AuthContextType = {
     user: Models.User<Models.Preferences> | null
-    signUp: (email: string, password: string) => Promise<void>
+    signUp: (email: string, password: string) => Promise<string | null>
     signIn: (email: string, password: string) => Promise<void>
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined)
 
 export function AuthProvider({children}: {children: React.ReactNode}){
+    const signUp = async (email: string, password: string) => {
+        try {
+            await account.create(ID.unique(), email, password)
+            await signIn(email, password)
+        } catch (error) {
+            
+        }
+    }
+
+     const signIn = async (email: string, password: string) => {
+        try {
+            await account.create(ID.unique(), email, password)
+        } catch (error) {
+            
+        }
+    }
+
     return (
-        <AuthContext.Provider
-        value={{user, signUp, signIn}}
-        ></AuthContext.Provider>
+        <AuthContext.Provider value={{user, signUp, signIn}}>
+            {children}
+        </AuthContext.Provider>
     )
 }
 
