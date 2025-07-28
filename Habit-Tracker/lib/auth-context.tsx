c3805b9695c5cab4724832ed0a61dad620ca073c
davidojo1144 <davco1144@gmail.com>
2025-07-28 06:30:09 +0100

    Habit Tracking Mobile App

--- Habit-Tracker/lib/auth-context.tsx
+++ Habit-Tracker/lib/auth-context.tsx
@@ -5,7 +5,7 @@ import { account } from "./appwrite";
 type AuthContextType = {
     user: Models.User<Models.Preferences> | null
     signUp: (email: string, password: string) => Promise<string | null>
-    signIn: (email: string, password: string) => Promise<void>
+    signIn: (email: string, password: string) => Promise<string | null>
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined)
@@ -15,16 +15,24 @@ export function AuthProvider({children}: {children: React.ReactNode}){
         try {
             await account.create(ID.unique(), email, password)
             await signIn(email, password)
+            return null
         } catch (error) {
-            
+            if (error instanceof Error) {
+                return error.message
+            }
+            return "An error occured during signup"
         }
     }
 
      const signIn = async (email: string, password: string) => {
         try {
-            await account.create(ID.unique(), email, password)
+            await account.createEmailPasswordSession(email, password)
+            return null
         } catch (error) {
-            
+            if (error instanceof Error) {
+                return error.message
+            }
+            return "An error occured during signup"
         }
     }
 
