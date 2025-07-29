1c58fce778ca765222b126329ab1a654f15f16e4
davidojo1144 <davco1144@gmail.com>
2025-07-29 21:12:47 +0100

    Habit Tracker Mobile App

--- Habit-Tracker/app/(tabs)/addhabit.tsx
+++ Habit-Tracker/app/(tabs)/addhabit.tsx
@@ -3,6 +3,7 @@ import { RFValue } from "react-native-responsive-fontsize";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
 import { Button, SegmentedButtons, TextInput } from "react-native-paper"
 import { useState } from "react";
+import { useAuth } from "@/lib/auth-context";
 
 const FREQUENCIES = ["daily", "weekly", "monthly"]
 type frequency = (typeof FREQUENCIES)[number]
@@ -11,13 +12,30 @@ export default function AddHabitScreen(){
     const [title, setTitle] = useState<string>("")
     const [description, setDescription] = useState<string>("")
     const [frequency, setFrequency] = useState<frequency>("daily")
+    const {user} = useAuth()
+
+    const handleSubmit = () => {
+        if (!user) return
+    }
 
     return (
         <View style={styles.container}>
-            <TextInput style={styles.input} label="Title" mode="outlined"/>
-            <TextInput style={styles.input} label="Description" mode="outlined"/>
+            <TextInput 
+            style={styles.input} 
+            label="Title" 
+            mode="outlined"
+            onChangeText={setTitle}
+            />
+            <TextInput 
+            style={styles.input} 
+            label="Description" 
+            mode="outlined"
+            onChangeText={setDescription}
+            />
             <View>
                 <SegmentedButtons
+                value={frequency}
+                onValueChange={(value) => setFrequency(value as frequency)}
                 buttons={
                     FREQUENCIES.map((freq) => ({
                         value: freq,
@@ -25,7 +43,7 @@ export default function AddHabitScreen(){
                 }))}
                 />
             </View>
-            <Button style={styles.button} mode="contained">Add Habits</Button>
+            <Button onPress={handleSubmit} disabled={!title || !description} style={styles.button} mode="contained">Add Habits</Button>
         </View>
     )
 }
