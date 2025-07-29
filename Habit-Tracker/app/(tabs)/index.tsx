a9332fc8679978ad2c07d9574d9e06c3c68cdbbf
davidojo1144 <davco1144@gmail.com>
2025-07-29 22:12:54 +0100

    Habit Tracker Mobile App

--- Habit-Tracker/app/(tabs)/index.tsx
+++ Habit-Tracker/app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
 
 import { DATABASE_ID, databases, HABIT_COLLECTION_ID } from "@/lib/appwrite";
 import { useAuth } from "@/lib/auth-context";
+import { Habit } from "@/types/database.type";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { Query } from "react-native-appwrite";
 import { Button } from "react-native-paper";
@@ -9,6 +11,12 @@ import { Button } from "react-native-paper";
 export default function Index() {
   const {signOut, user} = useAuth()
 
+  const [habits, setHabits] = useState<Habit[]>()
+
+  useEffect(() => {
+    fetchHabits()
+  }, [user])
+
   const fetchHabits = async () => {
     try {
       const response = await databases.listDocuments(
@@ -16,6 +24,7 @@ export default function Index() {
         HABIT_COLLECTION_ID,
         [Query.equal("user_id", user?.$id ?? "")]
       )
+      setHabits(response.documents as Habit[])
     } catch (error) {
       console.error(error)
     }
