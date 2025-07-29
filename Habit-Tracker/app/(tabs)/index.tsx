a0b22c50e50e33116951ea3de1d5e827f4dafe6c
davidojo1144 <davco1144@gmail.com>
2025-07-29 22:55:08 +0100

    Habit Tracker Mobile App

--- Habit-Tracker/app/(tabs)/index.tsx
+++ Habit-Tracker/app/(tabs)/index.tsx
@@ -8,6 +8,7 @@ import { Query } from "react-native-appwrite";
 import { Button } from "react-native-paper";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
 import { RFValue } from "react-native-responsive-fontsize";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 
 export default function Index() {
@@ -41,7 +42,7 @@ export default function Index() {
         mode="text" 
         icon={"logout"}>Sign out</Button>
       </View>
-
+      <View style={styles.content}>
       {
         habits?.length === 0 ? (
           <View>
@@ -49,12 +50,25 @@ export default function Index() {
           </View>
         ) : (
           habits?.map((habit, key) => (
-            <View key={key}>
-
+            <View style={styles.wrapper} key={key}>
+              <Text>{habit.title}</Text>
+              <Text>{habit.description}</Text>
+              <View>
+                <MaterialCommunityIcons 
+                  name="fire"
+                  size={28}
+                  color={"#ff9800"}
+                />
+              </View>
+              <Text>{habit.streak_count} day streak</Text>
+              <View>
+                <Text>{habit.frequency.charAt(0).toUpperCase() + habit.frequency.slice(1)}</Text>
+              </View>
             </View>
           ))
         )
       }
+      </View>
     </View>
   )
 }
@@ -63,18 +77,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: wp(3),
-    justifyContent: "center",
-    alignItems: "center"
+    //justifyContent: "center",
+    //alignItems: "center"
   },
   headline: {
     fontSize: RFValue(25)
   },
   habittext: {
     fontSize: RFValue(15),
-    marginTop: hp(2)
+    marginTop: hp(2),
   },
   button: {
     backgroundColor: "blue",
-    marginTop: hp(1)
-  }
+    marginTop: hp(1),
+    width: wp(40)
+  },
+  content: {
+    gap: wp(2)
+  },
+ wrapper: {
+  border: '#e0e0e0', 
+  borderRadius: wp(3), 
+  padding: wp(3), 
+  backgroundColor: '#ffffff',
+  boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)', 
+  marginTop: hp(2), 
+  transition: 'all 0.3s ease',
+  ':hover': {
+    boxShadow: '0 6px 16px rgba(0, 0, 0, 0.15)', 
+    transform: 'translateY(-2px)', 
+  },
+}
 })
