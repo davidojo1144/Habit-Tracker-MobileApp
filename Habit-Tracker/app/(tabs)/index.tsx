35030ab6d5fd85b9424d547a7707da658c2bc751
davidojo1144 <davco1144@gmail.com>
2025-07-29 22:25:20 +0100

    Habit Tracker Mobile App

--- Habit-Tracker/app/(tabs)/index.tsx
+++ Habit-Tracker/app/(tabs)/index.tsx
@@ -3,9 +3,11 @@ import { DATABASE_ID, databases, HABIT_COLLECTION_ID } from "@/lib/appwrite";
 import { useAuth } from "@/lib/auth-context";
 import { Habit } from "@/types/database.type";
 import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Query } from "react-native-appwrite";
 import { Button } from "react-native-paper";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
+import { RFValue } from "react-native-responsive-fontsize";
 
 
 export default function Index() {
@@ -30,15 +32,37 @@ export default function Index() {
     }
   }
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>index screen</Text>
-      <Button onPress={signOut} mode="text" icon={"logout"}>Sign out</Button>
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.headline}>Today's Habits</Text>
+        <Button onPress={signOut} mode="text" icon={"logout"}>Sign out</Button>
+      </View>
+
+      {
+        habits?.length === 0 ? (
+          <View>
+            <Text>Theres no habit, Create your first habit</Text>
+          </View>
+        ) : (
+          habits?.map((habit, key) => (
+            <View key={key}>
+
+            </View>
+          ))
+        )
+      }
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: wp(3),
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  headline: {
+    fontSize: RFValue(20)
+  }
+})
